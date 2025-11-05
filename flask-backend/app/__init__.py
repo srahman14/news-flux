014ae4e669e9d210b9125f9a7f61934aa57fbbc0
srahman14 <s.rahman14@hotmail.com>
2025-11-05 20:06:55 +0000

    added news route

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+import os
 
 def create_app():
     load_dotenv()
 
     app = Flask(__name__)
     CORS(app)
+
+    # Load API key example
+    app.config["NEWS_API_KEY"] = os.getenv("NEWS_API_KEY")
 
     # Example
     from app.routes.news_routes import news_bp

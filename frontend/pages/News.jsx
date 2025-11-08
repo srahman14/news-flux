e36d1d56975f553fe955d8ee833672ef5a278e74
Sayed <s.rahman14@hotmail.com>
2025-11-08 23:41:12 +0000

    news page

---
+++ frontend/pages/News.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NewsFeed from "../src/components/NewsFeed";
+
+const News = () => {
+  return (
+    <div className="min-h-screen">
+      <NewsFeed />
+    </div>
+  );
+};
+
+export default News;

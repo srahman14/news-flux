5cb2dc3edfde68db3dbe04dcb085550c30963a37
srahman14 <s.rahman14@hotmail.com>
2026-05-18 21:31:31 +0100

    upgrade axios version to 1.16.1

--- frontend/src/components/CryptoSmall.jsx
+++ frontend/src/components/CryptoSmall.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ArrowRight } from "lucide-react";
+import { API_BASE_URL } from "../lib/api";
 
 const CryptoSmall = () => {
   const [topCryptos, setTopCryptos] = useState([]);
